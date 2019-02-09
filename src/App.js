80bdf1595f4c455ca054bf5b047b02d21fabe580
Matt Cool <matthewbcool@gmail.com>
2019-02-08 21:38:11 -0600

    added react-youtube and basic jojo object to draw videos from

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import YouTube from 'react-youtube'
+import { phrases } from './joJoWordList'
+import './App.css'
 
 class App extends Component {
+  state = {
+    playOneId: phrases.DO_YOU_UNDERSTAND.id,
+    playOneStart: phrases.DO_YOU_UNDERSTAND.start,
+    playOneEnd: phrases.DO_YOU_UNDERSTAND.end,
+    playTwoId: phrases.HELL_TO_YOU.id,
+    playTwoStart: phrases.HELL_TO_YOU.start,
+    playTwoEnd: phrases.HELL_TO_YOU.end
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <YouTube
+          videoId={this.state.playOneId}
+          opts={{
+            height: '390',
+            width: '640',
+            playerVars: {
+              autoplay: 1,
+              start: this.state.playOneStart,
+              end: this.state.playOneEnd
+            }
+          }}
+          onReady={this._onReady}
+        />
+
+        <YouTube
+          videoId={this.state.playTwoId}
+          opts={{
+            height: '390',
+            width: '640',
+            playerVars: {
+              autoplay: 1,
+              start: this.state.playTwoStart,
+              end: this.state.playTwoEnd
+            }
+          }}
+          onReady={this._onReady}
+        />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
