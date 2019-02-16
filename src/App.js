02d5a061f4ae7bd170c77790b8a389ae7e17e007
Matt Cool <matthewbcool@gmail.com>
2019-02-16 14:05:05 -0600

    replaced toggle video with a hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import YouTube from 'react-youtube'
 import { phrases } from './joJoWordList'
 import './App.css'
 
-class App extends Component {
-  state = {
-    showVideos: false,
-    currentVideo: '',
-    nextVideo: '',
-    queuedVideo: '',
-    videoQueueIndex: 1
-  }
-  makeNewVideo = (id, start, end) => {
+function App() {
+  const [toggleVideos, setVideoVis] = useState(false)
+
+  // const [currentVideo, setCurrentVid] = useState({})
+
+  /*
+  setCurrentVid = (id, start, end) => {
     return (
       <YouTube
         videoId={id}
@@ -29,60 +27,16 @@ class App extends Component {
         onEnd={() => this.moveVideoUpQueue()}
       />
     )
-  }
+  } */
 
-  componentDidMount() {
-    //set the current video equal to the 1st element in vid list
-  }
-
-  moveVideoUpQueue = () => {}
-
-  incrementQueueIndex = (prevState, initializeVideoList) => {
-    console.log(this.state.videoQueueIndex)
-    if (prevState.videoQueueIndex >= initializeVideoList.length) {
-      return null
-    } else {
-      return {
-        videoQueueIndex: prevState.videoQueueIndex + 1,
-        currentVideo: this.state.nextVideo
-      }
-    }
-  }
-
-  _onReady(event) {
-    //event fires when the video first loads I think
-  }
-
-  _onStateChange(event) {
-    if (event.target.getPlayerState() === 0) {
-      //use this for potential performance enhancements
-    }
-  }
-  showVideos = () => {
-    let initializeVideoList = Object.values(phrases)
-    if (this.state.showVideos === false) {
-      this.setState({ showVideos: true })
-      this.setState({
-        currentVideo: this.makeNewVideo(
-          initializeVideoList[0].id,
-          initializeVideoList[0].start,
-          initializeVideoList[0].end
-        )
-      })
-    } else {
-      this.setState({ showVideos: false })
-      this.setState({ currentVideo: '' })
-    }
-  }
-
-  render() {
-    return (
-      <div className='App'>
-        <button onClick={this.showVideos}>RUN THIS MADNESS!</button>
-        {this.state.showVideos ? this.state.currentVideo : null}
-      </div>
-    )
-  }
+  return (
+    <div className='App'>
+      <button onClick={() => setVideoVis(!toggleVideos)}>
+        RUN THIS MADNESS!
+      </button>
+      {toggleVideos ? 'VIDEO PLAYING' : null}
+    </div>
+  )
 }
 
 export default App
